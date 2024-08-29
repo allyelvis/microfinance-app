846d47b82b272ed666e24441aff7e0c6a80c8850
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-08-29 22:04:14 +0000

    Build

--- replit.nix
+++ replit.nix
@@ -1,5 +1,13 @@
 { pkgs }: {
   deps = [
+    pkgs.plasma5Packages.kmousetool
+    pkgs.home-assistant-component-tests.kostal_plenticore
+    pkgs.kotlin-interactive-shell
+    pkgs.kotlin-native
+    pkgs.kotlin-language-server
+    pkgs.kotlin
+    pkgs.vimPlugins.kotlin-vim
+    pkgs.gradle
     pkgs.flutter316
     pkgs.cowsay
   ];
